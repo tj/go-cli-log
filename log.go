77dd6d603c28d3eb2c0b5efb2b54ce4c18bf276d
TJ Holowaychuk <tj@vision-media.ca>
2014-11-24 13:33:30 -0800

    docs

--- log.go
+++ log.go
@@ -2,31 +2,19 @@ package log
 
 import "fmt"
 
-//
-// Toggle verbose logging.
-//
-
+// Verbose enables debug.
 var Verbose = false
 
-//
-// Key width.
-//
-
+// Width of column.
 var Width = "14"
 
-//
 // Info.
-//
-
 func Info(key string, format string, args ...interface{}) {
 	p := fmt.Sprintf("\033[36m%"+Width+"s \033[90m:\033[0m ", key)
 	fmt.Printf(p+format+"\n", args...)
 }
 
-//
 // Debug.
-//
-
 func Debug(key string, format string, args ...interface{}) {
 	if Verbose {
 		p := fmt.Sprintf("\033[96m%"+Width+"s \033[90m:\033[0m ", key)
@@ -34,19 +22,13 @@ func Debug(key string, format string, args ...interface{}) {
 	}
 }
 
-//
 // Warning.
-//
-
 func Warn(format string, args ...interface{}) {
 	p := fmt.Sprintf("\033[33m%"+Width+"s \033[90m:\033[0m ", "warn")
 	fmt.Printf(p+format+"\n", args...)
 }
 
-//
 // Error.
-//
-
 func Error(err error) {
 	fmt.Printf("\033[31m%"+Width+"s \033[90m:\033[0m %s\n", "error", err)
 }
